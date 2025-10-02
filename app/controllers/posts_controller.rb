c5f182f4d7414daee5adb570190c66e076b6e1f7
MaruYuki0927 <211195289+MaruYuki0927@users.noreply.github.com>
2025-10-02 22:41:08 +0900

    11-8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.save
-    redirect_to '/top'
+    redirect_to post_path(post.id)
   end
 
   def index
@@ -16,6 +16,16 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end 
 
   private
   def post_params
